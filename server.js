943e24b0236cfe75447d39c104504cdf082f2ca9
Alan Leverenz <awleverenz@aol.com>
2020-05-12 16:12:28 -0400

    added client build folder, set config to production

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
@@ -22,6 +23,14 @@ if(process.env.NODE_ENV) {
 
 // user the api route for transactions
 app.use('/api/v1/transactions', transactions);
+
+// set path to client build folder if in production
+// this handles client paths after api/v1/transactions are handled
+if(process.env.NODE_ENV ===  'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
